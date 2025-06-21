67dc7966eddffe690e40ff8f895890f224575663
aki <lala.akira@gmail.com>
2025-06-22 06:41:11 +0900

    feat: Update DataMigrationOperator for deprecation warning

--- lib/dekiru/data_migration_operator.rb
+++ lib/dekiru/data_migration_operator.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "data_migration/operator"
+require "active_support/deprecation"
 
 module Dekiru
-  # 後方互換性のためのエイリアス
-  # 新しいコードでは Dekiru::DataMigration::Operator を使用してください
-  DataMigrationOperator = DataMigration::Operator
+  # Alias for backward compatibility
+  # Please use Dekiru::DataMigration::Operator in new code
+  class DataMigrationOperator < DataMigration::Operator
+    def self.new(...)
+      ActiveSupport::Deprecation.new.warn(
+        "Dekiru::DataMigrationOperator is deprecated. " \
+        "Use Dekiru::DataMigration::Operator instead."
+      )
+      DataMigration::Operator.new(...)
+    end
+  end
 end
